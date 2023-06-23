9f31d40b044009068fd8e705ba114965a45534dd
Kha <kha.minh1452@gmail.com>
2023-06-23 11:50:57 +0700

    f

--- onlinelearning/src/main/java/group5/swp391/onlinelearning/service/IUserService.java
+++ onlinelearning/src/main/java/group5/swp391/onlinelearning/service/IUserService.java
@@ -6,6 +6,11 @@ import group5.swp391.onlinelearning.entity.User;
 import group5.swp391.onlinelearning.model.user.dto.UserDTOLoginRequest;
 
 public interface IUserService {
-    boolean loginStudent(UserDTOLoginRequest student);
-    List<User> getAllUsers();
+    public Collection<User> getAllUsers();
+
+    public Collection<UserDTOAccountRequest> getAllUserDTOAccountRequest();
+
+    public User getUserById(int id);
+
+    public void changeStatus(int id);
 }
