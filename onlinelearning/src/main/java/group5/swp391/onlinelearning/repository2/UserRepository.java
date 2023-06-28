653937803dd5e742e51f36db89c052ea60bac00d
leanthai02 <leanthai02@gmail.com>
2023-06-29 01:45:03 +0700

    ONVID: update login + cart student side

--- onlinelearning/src/main/java/group5/swp391/onlinelearning/repository2/UserRepository.java
+++ onlinelearning/src/main/java/group5/swp391/onlinelearning/repository2/UserRepository.java
@@ -1,7 +1,5 @@
 package group5.swp391.onlinelearning.repository2;
 
-onlinelearning/src/main/java/group5/swp391/onlinelearning/repository2/UserRepository.java
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import group5.swp391.onlinelearning.entity.User;
