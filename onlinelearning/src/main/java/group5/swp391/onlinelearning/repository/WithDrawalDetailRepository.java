7c46b9a647e6734d9f0eec181ff8389134d26bf3
Nguyen Minh Hung <hackinghungtx@gmail.com>
2023-07-16 21:36:04 +0700

    Done WithDrawral NOT TEST

--- onlinelearning/src/main/java/group5/swp391/onlinelearning/repository/WithDrawalDetailRepository.java
+++ onlinelearning/src/main/java/group5/swp391/onlinelearning/repository/WithDrawalDetailRepository.java
@@ -6,11 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import group5.swp391.onlinelearning.entity.Wallet;
 import group5.swp391.onlinelearning.entity.WithdrawalDetail;
 
 @Repository
-public interface WithDrawalDetailRepository extends JpaRepository <WithdrawalDetail, Integer> {
-    @Query(value = "SELECT * FROM swp391_onvid.withdrawaldetail where teacher_id = ?1", nativeQuery = true)
+public interface WithDrawalDetailRepository extends JpaRepository<WithdrawalDetail, Integer> {
+    @Query(value = "SELECT * FROM swp391_onvid.withdrawal_detail where teacher_id = ?1", nativeQuery = true)
     public List<WithdrawalDetail> findByTeacherId(int studentId);
 }
