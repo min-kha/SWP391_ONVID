96db691c06eafe108447b3be8be7e447eab231b1
Nguyen Minh Hung <hackinghungtx@gmail.com>
2023-07-20 02:09:56 +0700

    Addition Home Page Front End

--- onlinelearning/src/main/java/group5/swp391/onlinelearning/repository/WithDrawalDetailRepository.java
+++ onlinelearning/src/main/java/group5/swp391/onlinelearning/repository/WithDrawalDetailRepository.java
@@ -1,5 +1,6 @@
 package group5.swp391.onlinelearning.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,11 @@ import group5.swp391.onlinelearning.entity.WithdrawalDetail;
 public interface WithDrawalDetailRepository extends JpaRepository<WithdrawalDetail, Integer> {
     @Query(value = "SELECT * FROM swp391_onvid.withdrawal_detail where teacher_id = ?1", nativeQuery = true)
     public List<WithdrawalDetail> findByTeacherId(int studentId);
+
+    @Query(value = "SELECT * FROM swp391_onvid.withdrawal_detail where teacher_id = ?1 AND MONTH(date) = MONTH(CURDATE()) AND YEAR(date) = YEAR(CURDATE());", nativeQuery = true)
+    public List<WithdrawalDetail> getRevenueByMonth(int studentId);
+
+    @Query(value = "SELECT * FROM swp391_onvid.withdrawal_detail where teacher_id = ?1 date >= DATE_SUB(DATE_FORMAT(CURDATE(), '%Y-%m-01'), INTERVAL 1 MONTH) AND date <= LAST_DAY(DATE_SUB(CURDATE(), INTERVAL 1 MONTH));", nativeQuery = true)
+    public List<WithdrawalDetail> getRevenueByMonthBefore(int studentId);
+
 }
