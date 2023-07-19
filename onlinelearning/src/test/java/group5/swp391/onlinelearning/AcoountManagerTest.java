a0eee008cf2f20257cf78f0c99f77b62af434633
leanthai02 <leanthai02@gmail.com>
2023-07-19 21:28:34 +0700

    aaa

--- onlinelearning/src/test/java/group5/swp391/onlinelearning/AcoountManagerTest.java
+++ onlinelearning/src/test/java/group5/swp391/onlinelearning/AcoountManagerTest.java
@@ -1,13 +1,10 @@
 package group5.swp391.onlinelearning;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
