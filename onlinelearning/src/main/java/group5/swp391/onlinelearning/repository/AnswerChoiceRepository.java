efd2d4b888f79ea30940155acf1ed8b77796fe31
leanthai02 <leanthai02@gmail.com>
2023-07-26 01:32:18 +0700

    commit fix

--- onlinelearning/src/main/java/group5/swp391/onlinelearning/repository/AnswerChoiceRepository.java
+++ onlinelearning/src/main/java/group5/swp391/onlinelearning/repository/AnswerChoiceRepository.java
@@ -1,24 +1,10 @@
-
 package group5.swp391.onlinelearning.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import group5.swp391.onlinelearning.entity.AnswerChoice;
-
-public interface AnswerChoiceRepository extends JpaRepository<AnswerChoice, Integer> {
-    @Query(value = "SELECT * FROM answer_choice WHERE answer = ?1 AND question_id = ?2", nativeQuery = true)
-    public AnswerChoice getIdByAnswer(String answer, int questionId);
-
-package group5.swp391.onlinelearning.repository;
-
 import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import group5.swp391.onlinelearning.entity.AnswerChoice;
 
 public interface AnswerChoiceRepository extends JpaRepository<AnswerChoice, Integer> {
     @Modifying
@@ -28,5 +14,6 @@ public interface AnswerChoiceRepository extends JpaRepository<AnswerChoice, Inte
     @Query(value = "Select * from swp391_onvid.answer_choice WHERE question_id = ?1", nativeQuery = true)
     public List<AnswerChoice> getAsByQuestionId(int id);
 
-
+    @Query(value = "SELECT * FROM answer_choice WHERE answer = ?1 AND question_id = ?2", nativeQuery = true)
+    public AnswerChoice getIdByAnswer(String answer, int questionId);
 }
